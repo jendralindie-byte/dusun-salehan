4bd2edba6d0a78f72d7d5575d5bc32207c9f8ef9
jendralindie-byte <jendralindie@gmail.com>
2026-07-13 13:34:02 +0700

    Add files via upload

---
+++ Dusun Salehan/js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://vufymausskugbgnrtinx.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_hbGzKC1A9JcXCMx-nCpZVQ_MT3tJLwT";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
